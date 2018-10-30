bae1538554976d9e64ba2004f666c4276a41c99c
joshsager <sager379@gmail.com>
2018-10-30 07:19:31 -0400

    Fixes typo

--- functional_tests.py
+++ functional_tests.py
@@ -8,13 +8,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_cat_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith hs heard about a cool new online to-do app. She goes
         # to check out it's homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the pate title and header mention to-do-lists
-        self.assertIn('To-Do', browser.title) 
+        self.assertIn('To-Do', self.browser.title) 
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
